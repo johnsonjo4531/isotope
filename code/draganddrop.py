4c4ef3619545d10ef7215e01ddb2abd2df83b593
Mitch Shelton <>
2018-08-24 23:49:27 -0600

    changed default size and position, added status bar and  background image

--- code/draganddrop.py
+++ code/draganddrop.py
@@ -27,35 +27,70 @@ from PyQt5.QtCore import *
 #         self.setText(e.mimeData().text()) 
 
 
-class Example(QWidget):
+class App(QMainWindow):
   
     def __init__(self):
         super().__init__()
         
         self.initUI()
         
+    ######Code for file explorer for manual import features
+    # def openFileNameDialog(self):    
+    #     options = QFileDialog.Options()
+    #     options |= QFileDialog.DontUseNativeDialog
+    #     fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+    #     if fileName:
+    #         print(fileName)
+ 
+    # def openFileNamesDialog(self):    
+    #     options = QFileDialog.Options()
+    #     options |= QFileDialog.DontUseNativeDialog
+    #     files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
+    #     if files:
+    #         print(files)
+ 
+    # def saveFileDialog(self):    
+    #     options = QFileDialog.Options()
+    #     options |= QFileDialog.DontUseNativeDialog
+    #     fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
+    #     if fileName:
+    #         print(fileName)
         
     def initUI(self):
 
+        self.setWindowTitle('IsoType')
+        self.setGeometry(500, 250, 800, 600)
+
+        backlabel = QLabel(self)
+        pixmap = QPixmap('../images/background-1.png')
+        backlabel.setPixmap(pixmap)
+
         label1 = QLabel(self)
         label1.setText("Drag")
-        label1.move(130, 40)
+        label1.move(380, 200)
 
         label2 = QLabel(self)
         label2.setText("A")
-        label2.move(140, 65)
+        label2.move(390, 240)
 
         label3 = QLabel(self)
         label3.setText("File")
-        label3.move(135, 90)
-        
-        self.setWindowTitle('IsoType')
-        self.setGeometry(300, 300, 300, 150)
+        label3.move(385, 280)
+
+        message = "Awaiting file..."
+        self.statusBar().showMessage(message)
+
+        #####Code for import window
+        # self.openFileNameDialog()
+        # self.openFileNamesDialog()
+        # self.saveFileDialog()
+
+        self.show()
 
 
 if __name__ == '__main__':
   
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = App()
     ex.show()
     app.exec_()  
